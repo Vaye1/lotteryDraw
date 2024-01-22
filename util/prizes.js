03297060139fff64f4e009063e0cb3e84434ec13
Vaye1 <30649124+Vaye1@users.noreply.github.com>
2024-01-22 19:54:52 +0800

    updata

---
+++ util/prizes.js
@@ -0,0 +1,46 @@
+空气炸锅5L
+披肩按摩器
+手磨咖啡套装
+一壶三杯茶具套装
+红色龙年抱枕
+香薰蜡烛新年礼盒
+泡面锅
+青砂石狮子桌面摆件
+龙年帆布包
+存钱罐
+
+
+let aaa =  [
+    {
+        name: "存钱罐", prize: "四等奖 ", level: 4, "url": "img/prize/a10.png", person: ""
+    },
+    {
+        name: "龙年帆布包", prize: "四等奖 ", level: 4, "url": "img/prize/a9.png", person: ""
+    },
+    {
+        name: "青砂石狮子桌面摆件", prize: "四等奖 ", level: 4, "url": "img/prize/a8.png", person: ""
+    },
+    {
+        name: "泡面锅", prize: "四等奖 ", level: 4, "url": "img/prize/a7.png", person: ""
+    },
+
+    {
+        name: "香薰蜡烛新年礼盒", prize: "三等奖 ", level: 3, "url": "img/prize/a6.png", person: ""
+    },
+    {
+        name: "红色龙年抱枕", prize: "三等奖 ", level: 3, "url": "img/prize/a5.png", person: ""
+    },
+    {
+        name: "一壶三杯茶具套装", prize: "三等奖 ", level: 3, "url": "img/prize/a4.png", person: ""
+    },
+
+    {
+        name: "手磨咖啡套装", prize: "二等奖", level: 2, "url": "img/prize/a3.png", person: ""
+    },
+    {
+        name: "披肩按摩器", prize: "二等奖", level: 2, "url": "img/prize/a2.png", person: ""
+    },
+    {
+        name: "空气炸锅5L", prize: "一等奖 ", level: 1, "url": "img/prize/a1.png", person: ""
+    }
+]
